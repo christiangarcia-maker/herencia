752d6039d3596f61a4d502441ba69f8eb465e395
christian.garcia <christian.garcia@106-07.info2>
2026-04-06 09:45:13 +0200

    .

--- src/ej4/Electrodomesticos.java
+++ src/ej4/Electrodomesticos.java
@@ -1,5 +1,32 @@
 package ej4;
 
 public class Electrodomesticos {
+	
+	public enum Color {
+		BLANCO,NEGRO,ROJO,AZUL,GRIS
+	}
+	
+	public enum ConsumoEnergetico {
+		A,B,C,D,E,F
+	}
+	
+	protected double precioBase = 100;
+	protected Color color = Color.BLANCO;
+	protected ConsumoEnergetico consumoEnergia = ConsumoEnergetico.F;
+	protected double peso = 5;
+	
+	public Electrodomesticos(double precioBase, double peso) {
+		this.precioBase = precioBase;
+		this.peso = peso;
+	}
 
+	public Electrodomesticos(double precioBase, String color, String consumoEnergia, double peso) {
+		this.precioBase = precioBase;
+		this.color = Color.valueOf(color.toUpperCase());
+		this.consumoEnergia = ConsumoEnergetico.valueOf(consumoEnergia.toUpperCase());
+		this.peso = peso;
+	}
+	
+	
+	
 }
