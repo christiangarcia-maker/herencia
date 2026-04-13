0ef5f7a9514f2b4fac65d91c170bb79fd0baedf4
christiangarcia-maker <christian.garcia@iesnervion.es>
2026-04-13 10:15:41 +0200

    ...

--- src/ej4/Electrodomesticos.java
+++ src/ej4/Electrodomesticos.java
@@ -2,29 +2,52 @@ package ej4;
 
 public class Electrodomesticos {
 	
-	public enum Color {
-		BLANCO,NEGRO,ROJO,AZUL,GRIS
-	}
+	protected double precioBase;
+	protected String color;
+	protected char consumoEnergetico;
+	protected double peso;
 	
-	public enum ConsumoEnergetico {
-		A,B,C,D,E,F
-	}
+	protected static final String COLOR_DEF = "Blanco";
+	protected static final char CONSUMO_ENERGETICO_DEF = 'F';
+	protected final double PRECIO_BASE_DEF = 100;
+	protected final double PESO_DEF = 5;
 	
-	protected double precioBase = 100;
-	protected Color color = Color.BLANCO;
-	protected ConsumoEnergetico consumoEnergia = ConsumoEnergetico.F;
-	protected double peso = 5;
+	private static final String[] COLORES_VALIDOS = { "blanco", "negro", "rojo", "azul", "gris" };
 	
 	public Electrodomesticos(double precioBase, double peso) {
 		this.precioBase = precioBase;
+		this.color = COLOR_DEF;
+		this.consumoEnergetico = CONSUMO_ENERGETICO_DEF;
 		this.peso = peso;
 	}
-
-	public Electrodomesticos(double precioBase, String color, String consumoEnergia, double peso) {
-		this.precioBase = precioBase;
-		this.color = Color.valueOf(color.toUpperCase());
-		this.consumoEnergia = ConsumoEnergetico.valueOf(consumoEnergia.toUpperCase());
-		this.peso = peso;
+	
+	public Electrodomesticos(double precioBase, String color, char consumoEnergetico, double peso) {
+        this.precioBase = precioBase;
+        this.color = comprobarColor(color);
+        this.consumoEnergetico = comprobarConsumoEnergetico(consumoEnergetico);
+        this.peso = peso;
+    }
+	
+	private char comprobarConsumoEnergetico (char letra) {
+		char letraUpper = Character.toUpperCase(letra);
+		if (letraUpper >= 'F' && letraUpper <= 'F') {
+			return letraUpper;
+		}
+		return CONSUMO_ENERGETICO_DEF;
+	}
+	
+	private String comprobarColor (String color) {
+		if (color == null) {
+			return COLOR_DEF;
+		}
+		
+		String colorNormalizado = color.toLowerCase();
+		for (String colorValido : COLORES_VALIDOS) {
+			if (colorValido.equals(colorNormalizado)) {
+				return colorNormalizado;
+			}
+		}
+		return COLOR_DEF;
 	}
 	
 	
